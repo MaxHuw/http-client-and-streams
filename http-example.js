f912d79bcd8f73aaa95fd8c4e8bff60c3f4884ff
Max Graham <m.huw.graham@gmail.com>
2019-01-12 22:22:19 +0000

    www.example.org does not seem to exist.

--- http-example.js
+++ http-example.js
@@ -1,3 +1,20 @@
 var https = require('https');
 
 console.log("I did it!");
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+//www.example.org does not seem to work.
+
+var callback = function() {
+  console.log("In response handler callback!");
+};
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I'va made the request!");
